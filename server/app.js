ec6db96d7653b005da29b0abffa1e71ac999f8f4
Ido Strauss <strauss02@hotmail.co.il>
2021-11-03 14:43:29 +0200

    refactor structure

--- server/app.js
+++ server/app.js
@@ -12,3 +12,4 @@ app.get('/', function (req, res) {
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
+//fdssdf
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './style.scss'
+
 const inputField = document.getElementById('url_input')
 const submitBtn = document.getElementById('submit-btn')
 
 submitBtn.addEventListener('click', handleSubmit)
-
+//fsdfs
 function handleSubmit(e) {
   const inputURL = inputField.value
   console.log(inputURL)
